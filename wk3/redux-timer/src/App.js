b7502b58e81efd9db27dc30e28fa4b9e833be2ad
caidoole <caitlin.dooley@bryanuniversity.edu>
2020-09-16 14:22:53 -0700

    wk3 past due submission

---
+++ wk3/redux-timer/src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./styles.css"
+import Header from "./components/Header"
+import Timer from "./timerComponents/Timer"
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Timer />
+        </div>
+    )
+}
+
+export default App
---
+++ wk3/redux-timer/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header className='header'>
+            <img className='image' src='https://i.pinimg.com/736x/e7/16/11/e71611654eb46006368ab7c0befdb0f9.jpg' alt='logo'/>
+            <h2 className='title'>Timer</h2>
+        </header>
+    )
+}
+
+export default Header
